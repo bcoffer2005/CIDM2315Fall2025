ed42be85f8babe4b80059fc04c159f54b20a90f2
bcoffer2005 <bcoffer1@buffs.wtamu.edu>
2025-08-31 17:09:39 -0500

    Submitted Homework1

---
+++ Homework1.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double X = 2.5;
+        double Y = 3.3;
+
+        double Z = 4 * X * X + 3 * Y;
+
+        Console.WriteLine($"For X = {X:F1} and Y = {Y:F1}");
+        Console.WriteLine("Answer of Homework1");
+    }
+}
