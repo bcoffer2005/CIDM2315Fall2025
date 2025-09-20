74ec2fe1d354d0b9f57d6ba070fc97ab63a091ac
bcoffer2005 <bcoffer1@buffs.wtamu.edu>
2025-09-20 17:48:01 -0500

    Add files via upload

---
+++ Program - Copy.cs
@@ -0,0 +1,24 @@
+﻿namespace Homework4;
+
+class Program
+{
+    static int LargestNumber(int a, int b)
+    {
+        if (a > b)
+            return a;
+        else
+            return b;
+    }
+
+    static void Main(string[] args)
+    {
+        int a = 3, b = 5;
+
+        // Call the method
+        int result = LargestNumber(a, b);
+
+        // Print the result
+        Console.WriteLine("a = {0}; b = {1}", a, b);
+        Console.WriteLine("The largest number is: {0}", result);
+    }
+}
